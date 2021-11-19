a2be1fb6777fb7dd653b1bc0d84689907404ef70
Huseyin Kaya <88791134+kayahuseyin64@users.noreply.github.com>
2021-11-19 13:42:51 -0600

    b

--- securitygroup.tf
+++ securitygroup.tf
@@ -33,7 +33,6 @@ resource "aws_security_group" "for_testing" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
